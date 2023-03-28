9b3ce474f5e857a892cb3b8abd25ad2971e787e9
JoeAttard90 <attard999@gmail.com>
2023-03-28 19:45:52 +0100

    task: built the ValidateNHSNumber receiver function which uses the modulus 11 algorithm to validate the number

--- pkg/validation/validation.go
+++ pkg/validation/validation.go
@@ -1,5 +1,10 @@
 package validation
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type Validator struct {
 }
 
@@ -11,7 +16,29 @@ func NewValidator() (*Validator, error) {
 // ValidateNHSNumber validates the nhs number for a patient using the modulus 11 algorithm. If the validation fails,
 // and error is returned
 func (v *Validator) ValidateNHSNumber(nhsNumber int) error {
-	panic("implement me")
+	numLength := len(strconv.Itoa(nhsNumber))
+	originalNum := nhsNumber
+	if numLength != 10 {
+		return fmt.Errorf("expected a digit of length 10, got digit of length - %d", numLength)
+	}
+
+	var sum int
+	checkDigit := nhsNumber % 10
+	nhsNumber /= 10
+	// Multiply each digit by the appropriate weighting factor
+	for i := 1; i < 10; i++ {
+		digit := nhsNumber % 10
+		weight := i + 1
+		sum += digit * weight
+		if nhsNumber >= 10 {
+			nhsNumber /= 10
+		}
+	}
+	checksum := 11 - (sum % 11)
+	if checksum != checkDigit {
+		return fmt.Errorf("the nhs number provided - %d did not meeet the validation criteria. Original : %d", nhsNumber, originalNum)
+	}
+	return nil
 }
 
 func (v *Validator) GenerateValidNHSNumber() int {
